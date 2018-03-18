bb44a851ac030bdd7a5411c4bdb12b738343c1c8
Bibhuti93 <bibhuti.mymail@gmail.com>
2018-03-18 13:37:16 +0530

    Update index.php

--- index.php
+++ index.php
@@ -2,13 +2,6 @@
 $method = $_SERVER['REQUEST_METHOD'];
 //$msg = "First line of text\nSecond line of text";
 
-// use wordwrap() if lines are longer than 70 characters
-//$msg = wordwrap($msg,70);
-
-// send email
-//mail("moscosisi@gmail.com","My subject",$msg);
-
-
 // Process only when method is POST
 if($method == 'POST'){
 
@@ -17,25 +10,7 @@ $json = json_decode($requestBody);
 
 $equis = $json->result->parameters->equis;
 
-switch ($equis) {
-    case 8:
-        $speech = "Hi, Nice to meet you";
-
-        break;
-
-    case 'bye':
-        $speech = "Bye, good night";
-        break;
-
-    case 'anything':
-        $speech = "Yes, you can type anything here.";
-        break;
-
-    default:
-        $speech = "Sorry, I didnt get that. Please ask me something 
-else.";
-        break;
-}
+$speech="Hello Bibhuti";
 
 
 $response = new \stdClass();
